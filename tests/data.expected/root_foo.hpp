3def1f08a80b7089e641a9df7f8565ab009922d5
David SPORN <sporniket.studio@gmail.com>
2024-10-18 06:52:28 +0200

    Fixes #1 : [gencpp] generate a blank cpp file and its header

---
+++ tests/data.expected/root_foo.hpp
@@ -0,0 +1,15 @@
+/****************************************
+
+---
+Copyright (C) 20xx Unknown author
+---
+ALL RIGHT RESERVED -- project 'Unknown project'
+****************************************/
+#ifndef FOO__HPP
+#define FOO__HPP
+// ================[ CODE BEGINS ]================
+
+// ...your code...
+
+// ================[ END OF CODE ]================
+#endif
---
+++ tests/data.expected/root_whatever.cpp
@@ -0,0 +1,10 @@
+/****************************************
+
+---
+Copyright (C) 20xx Unknown author
+---
+ALL RIGHT RESERVED -- project 'Unknown project'
+****************************************/
+#include "whatEver.hpp"
+
+// ...your code...
